8d3a4061618b413065a951b97023c415c2e0e5ae
danh <danh@lightsky.com>
2009-06-10 14:13:41 -0400

    Added second ruby script server alias, removed local hosting of personal wiki

--- script/start.sh
+++ script/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:$PATH"
 ln -s /opt/local/var/run/mysql5/mysqld.sock /tmp/mysql.sock
-cd /Users/danh/Dropbox/Work/Wiki && gitjour serve
 open /Applications/LightSky\ Webmail.app/
 open /Applications/Firefox.app
 open /Applications/VirtualBox.app
