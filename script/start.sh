d8e1a3eda160b6eb21fff19c4bba4961e3e87307
danh <danhorst@gmail.com>
2009-06-22 10:49:41 -0400

    Script cleanup

--- script/start.sh
+++ script/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:$PATH"
+export PATH="/bin:$PATH"
 ln -s /opt/local/var/run/mysql5/mysqld.sock /tmp/mysql.sock
 open /Applications/LightSky\ Webmail.app/
 open /Applications/Firefox.app
--- script/todo.sh
+++ script/todo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:$PATH"
+export PATH="/usr/bin:/bin:$PATH"
 mate ~/Dropbox/todo/ && mate ~/Dropbox/todo/work.tasks
