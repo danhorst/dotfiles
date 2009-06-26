db4478dab567c64464e733f8bbebd7315f4a457e
Jessica Brubaker Horst <jessicagb@jessicagb-desktop.(none)>
2009-06-26 08:55:19 -0400

    Removed mac-specific files

--- script/lock.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend
--- script/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export PATH="/bin:$PATH"
-ln -s /opt/local/var/run/mysql5/mysqld.sock /tmp/mysql.sock
-open /Applications/LightSky\ Webmail.app/
-open /Applications/Firefox.app
-open /Applications/VirtualBox.app
-open /Applications/Utilities/Terminal.app
