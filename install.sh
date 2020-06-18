05c54c9b1c94210a607149cf3108d5e04c6a3388
Dan Horst <danhorst@gmail.com>
2020-06-18 07:59:41 -0400

    Install Pathogen if it isn't already there

--- install.sh
+++ install.sh
@@ -18,3 +18,11 @@ ls -1 "$dotfiles_directory" | xargs -i ln -nsf "$dotfiles_directory/{}" "$HOME/.
 echo "Symlinking scripts & utilities into $HOME/bin"
 bin_directory="$(pwd)/bin"
 ls -1 "$bin_directory" | xargs -i ln -nsf "$bin_directory/{}" "$HOME/bin/{}"
+
+if [ -d "$HOME/.vim/bundle" ]; then
+  echo "Pathogen is already installed"
+else
+  echo "Installing Pathogen"
+  mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/bundle"
+  curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+fi
