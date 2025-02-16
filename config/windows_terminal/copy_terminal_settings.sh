cfed13d5257d9da240d47cad3d2a77fcc5b6abd7
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2025-02-16 15:19:53 -0500

    Back up Windows Terminal settings

--- config/windows_terminal/copy_terminal_settings.sh
+++ config/windows_terminal/copy_terminal_settings.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cp /mnt/c/Users/dbrubak1/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json git/dotfiles/config/windows_terminal/
+
+cp /mnt/c/Users/danbr/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json "$HOME/git/dotfiles/config/windows_terminal/"
