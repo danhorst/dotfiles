7eea3f7ad9d5bd9c8129a46438145ea52712f7a5
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2025-02-16 15:11:49 -0500

    Strip out old work stuff

--- WSL2/fix-net.bat
+++
@@ -1,3 +0,0 @@
-@echo OFF
-SET SCRIPT_PATH=C:\Users\%USERNAME%\WSL2
-powershell -ExecutionPolicy unrestricted -Command "& { cd '%SCRIPT_PATH%'; .\fix-net.ps1 }" < nul
--- WSL2/fix-net.ps1
+++
@@ -1,2 +0,0 @@
-Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} | Set-NetIPInterface -InterfaceMetric 4000
-Get-NetIPInterface -InterfaceAlias "vEthernet (WSL)" | Set-NetIPInterface -InterfaceMetric 1
